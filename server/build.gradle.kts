c32e30bebc0e8f9a98b5950f298282e1b81137e3
Gary <garyguo9@gmail.com>
2025-05-28 03:36:51 -0400

    v0.2.2

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.idunnololz.summitForLemmy.server"
-version = "0.0.1"
+version = "0.2.2"
 
 application {
   mainClass.set("com.idunnololz.summitForLemmy.server.ApplicationKt")
